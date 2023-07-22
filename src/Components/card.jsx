b4c8d8e367f20aa9f326dd85c0a0aae9209493d2
Harender Kumar <harenderk62@gmail.com>
2023-07-23 04:10:49 +0530

    Commit

---
+++ src/Components/card.jsx
@@ -0,0 +1,11 @@
+import "./component-css.scss"
+
+const Card =(props)=>{
+    return(<div className="card">
+        <img src={props.img} alt="program-Logo" />
+        <p className="cardTitle">{props.title}</p>
+        <p className="cardBody">{props.body}</p>
+    </div>)
+}
+
+export default Card;
